42c4afc694ab3983d096d4464fb7720bfcedad14
early code <68994565+earlycodetech@users.noreply.github.com>
2020-09-18 17:20:47 +0100

    Create simple_interest_work.py

---
+++ simple_interest_work.py
@@ -0,0 +1,11 @@
+principal = int(input('Enter the amount you are invest \n'))
+rate = int(input('Enter agreed interest rate \n'))
+time = int(input('How long are you investing? \n'))
+
+# rt = rate * time
+# re_rt = 1 + rt
+# final_amount = principal * re_rt
+
+# final_amount
+
+interest = (principal * time * rate) / 100
